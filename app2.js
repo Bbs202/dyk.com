69a3e8e562704345d096072851e64ce4cc2b1185
Bbs202 <bolajibidemi132@gmail.com>
2022-02-23 21:59:36 +0100

    updated index.html

--- app2.js
+++ app2.js
@@ -10,4 +10,14 @@ $(document).ready(function(){
         $(this).find("i").toggleClass("fa-moon fa-sun")      
    
     })
+    $("#btn1").click(function(){
+        var name1 = $("#name1").val();
+        var nextSite = $('#btn1').html(<a href="index2.html"></a>);
+        if(name1 == ""){
+            alert("enter your name");
+        }
+        else{
+            nextSite
+        }
+    })
 })
